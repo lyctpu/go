91a32fdbb2076337aece70ac20a037a63d88e89f
ЛицейТПУ <114549805+lyctpu@users.noreply.github.com>
2023-06-17 09:56:23 +0700

    Update 1.go

--- 1.go
+++ 1.go
@@ -3,10 +3,24 @@ package main
 import "fmt"
 
 func main() {
+    var b string
+    b = "Hello World"
+    
+    var c string = "hello"
+    const s string = "Hello World"
+    
     x:="Go"
     fmt.Println("Hello World", x)
     fmt.Println(len("Hello World"))
     fmt.Println("Hello World"[1])
     fmt.Println("Hello " + "World")
+}
+
+func logic{
+    fmt.Println(true && true)
+    fmt.Println(true && false)
+    fmt.Println(true || true)
+    fmt.Println(true || false)
+    fmt.Println(!true)
 }
 // go run hello.go
