61747581d446dc2ab12d357e4975e791f1d858b4
ЛицейТПУ <114549805+lyctpu@users.noreply.github.com>
2023-06-17 10:31:19 +0700

    Create 3.go

---
+++ 3.go
@@ -0,0 +1,14 @@
+
+var total float64 = 30
+x := [5]int{ 98, 93, 77, 82, 83 }
+fmt.Println(total / float64(len(x)))
+
+x_srez:= []int{ 98, 93, 77, 82, 83 }
+x_srez2 := make([]int, 2)
+copy(x_srez2, x_srez1)
+
+sr := x[0:5]
+slice2 := append(sr, 4, 5)
+fmt.Println(sr, slice2)
+
+
