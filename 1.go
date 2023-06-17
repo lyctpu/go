4d53d9045a4e54e80c4491d313b0a59e5d4c030a
ЛицейТПУ <114549805+lyctpu@users.noreply.github.com>
2023-06-17 09:52:20 +0700

    Update 1.go

--- 1.go
+++ 1.go
@@ -5,5 +5,8 @@ import "fmt"
 func main() {
     x:="Go"
     fmt.Println("Hello World", x)
+    fmt.Println(len("Hello World"))
+    fmt.Println("Hello World"[1])
+    fmt.Println("Hello " + "World")
 }
 // go run hello.go
