b953c42b4592dc98a9325b028d8e58d30940e8ec
ЛицейТПУ <114549805+lyctpu@users.noreply.github.com>
2023-06-17 10:32:15 +0700

    Update 3.go

--- 3.go
+++ 3.go
@@ -1,14 +1,18 @@
+package main
 
-var total float64 = 30
-x := [5]int{ 98, 93, 77, 82, 83 }
-fmt.Println(total / float64(len(x)))
+import "fmt"
 
-x_srez:= []int{ 98, 93, 77, 82, 83 }
-x_srez2 := make([]int, 2)
-copy(x_srez2, x_srez1)
+func main() {
+  var total float64 = 30
+  x := [5]int{ 98, 93, 77, 82, 83 }
+  fmt.Println(total / float64(len(x)))
 
-sr := x[0:5]
-slice2 := append(sr, 4, 5)
-fmt.Println(sr, slice2)
+  x_srez:= []int{ 98, 93, 77, 82, 83 }
+  x_srez2 := make([]int, 2)
+  copy(x_srez2, x_srez1)
 
+  sr := x[0:5]
+  slice2 := append(sr, 4, 5)
+  fmt.Println(sr, slice2)
 
+}
